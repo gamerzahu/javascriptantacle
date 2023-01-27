35777ef19fb42fd009bb3f8e085c7952360c97d2
Zah <franciscoantacle@gmail.com>
2023-01-26 23:53:39 -0300

    Se sube primer proyecto

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/** Prompt */  
+// edad
+const edad = prompt (Message="Ingrese su edad")
+if(edad>=18){   
+
+alert ("Sos bienvenido a la página")
+}
+    else{
+alert ("No tenes acceso") 
+    }
+
+//
+
+/** For */
+
+for(let edad = 0; edad > 18; edad++) {
+
+    let edad = prompt (Message="Ingrese su edad")
+
+    console.log("La edad ingresada es:" + edad)
+}
